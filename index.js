3a5d70a32c7635ea5aff15f65fbe7baf26f21c66
naymurweb <naymurweb@gmail.com>
2022-12-20 21:45:08 +0600

    vercel problem

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 const port = process.env.PORT || 7000;
+app.use(cors());
+app.use(express.json())
 
 const courses = require("./data/courses.json");
 
